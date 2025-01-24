335aeaed45d12a7b051f227956cd0e1f66b7a477
OrangeJellyfishCHEN <lotaustralin12@gmail.com>
2025-01-24 18:50:07 +0800

    tree

---
+++ 2025/Jan/LC236.py
@@ -0,0 +1,9 @@
+class LC236(object):
+    def lowestCommonAncestor(self, root, p, q):
+        if root is None or root == p or root == q:
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if left and right:
+            return root
+        return left or right
